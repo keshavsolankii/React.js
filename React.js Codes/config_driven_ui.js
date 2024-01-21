f46adedadc4f7112a806f6f104ecec25c0a0f211
Keshav <110564015+keshavsolankii@users.noreply.github.com>
2024-01-21 22:28:14 +0530

    Create config_driven_ui.js

---
+++ React.js Codes/config_driven_ui.js
@@ -0,0 +1,27 @@
+// This example is for restaurant listing
+const config = [
+  {
+    type = "carousel",
+    cards: [
+      {  offerName: "50% off",
+      },
+      {  offerName: "No Delivery charge",
+      },
+    ],
+  },
+  {
+    type = "Restaurants",
+    cards: [
+      {
+        name : "Burger King",
+        image : "link",
+        ratings : "4.2",
+      },
+      {
+        name : "KFC",
+        image : "link",
+        ratings : "4.0",
+      },
+    ],
+  },
+];
